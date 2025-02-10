7e3b5091fff0cfa39723ac4bd654fc32b76cb4ea
hero-Can <herocan789@gmail.com>
2025-02-10 16:40:06 +0100

    create migration & factory & seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -14,7 +14,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word(), // Generates a random word for the product name
+            'description' => $this->faker->sentence(10), // Generates a sentence for the product description
+            'price' => $this->faker->randomFloat(2, 1, 1000), // Generates a random float for the price (2 decimal points)
+            'image' => $this->faker->imageUrl(), // Generates a random image URL
+            'stock' => $this->faker->numberBetween(1, 100), // Generates a random number for the stock count
         ];
     }
 }
